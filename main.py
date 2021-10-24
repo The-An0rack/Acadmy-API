d3473e7e6cdc4aa92d306170b21ef82e378064b1
Kartik <kartikbhatt2000@gmail.com>
2021-10-24 11:17:53 +0530

    Profile Section Making

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+class profile(Resource):
+   def get(self):
+        return("Name:-" "Dewashish Mehta" 
+        "Email:-" "Dewmehta99@gmail.com" "Goal:-" "GATE CS")
+
+api.add_resource(profile,"/profile")
+
+if __name__ == "__main__":
+    app.run(debug=True)
